d8f6fa14cd597b4728256e3be7203b9174329aee
supawer0728 <supawer0728@gmail.com>
2018-03-21 20:33:46 +0900

    first commit

---
+++ src/main/java/com/parfait/study/simplemultitx/SimpleMultiTxApplication.java
@@ -0,0 +1,12 @@
+package com.parfait.study.simplemultitx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMultiTxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMultiTxApplication.class, args);
+	}
+}
